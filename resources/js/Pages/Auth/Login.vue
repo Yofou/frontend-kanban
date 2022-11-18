ed88b021588fff6d77fb9408167765cf128e129e
Yofou <exodias1965@gmail.com>
2022-11-18 01:35:58 +0000

    init commit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,35 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head, useForm } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canResetPassword: Boolean,
+    status: String,
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <GuestLayout>
+        <Head title="Log in" />
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <h2>hi</h2>
+        </form>
+    </GuestLayout>
+</template>
